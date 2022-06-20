1118f2ab6c4e9a6a1e11caee2f891d415a7302ba
TechTalkerShubh <jitiya66@gmail.com>
2022-06-20 15:03:44 +0530

    Cleaning up

--- routes/web.php
+++ routes/web.php
@@ -12,33 +12,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-//Send data to view and access there
-Route::get('/demo/{name}/{id?}', function($name, $id = null){ 
-   $data = compact('name', 'id'); //Array, takes var names as parameter
-   return view('demo')->with($data);
-});
-
-
-Route::post('/test', function(){
-    echo "can't be accessed, can be used with form & postman";
-});
-
-Route::any('/getpost', function(){
-    echo "can be get or post, not a valid method - provided by laravel";
-});
-Route::put('/getpost', function(){
-    echo "Other methods are put, patch & delete";
-});
-
-/* 
-1. Routing files
-2. How to manage
-3. Custom routes
-*/
-
-
